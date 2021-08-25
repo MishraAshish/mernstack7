f0a12b4449fa21145a0b56c47864cfe3d35c88d1
Ashish <ash.ishu025g@gmail.com>
2021-08-25 21:26:50 +0530

    questions

---
+++ corejs/programmingQuestions.js
@@ -0,0 +1,9 @@
+// Practice Questions - 
+//=> create an example of print function which contains a callback function and prints user details, where user is an objet with name, age, address, exepertise
+//=> the same callback function should be used to print account details - account name, number, balance, account type, branch
+//=> create a class(Through Object or Constructor Function) Name vaccine which contains various properties like name, brand, efficay, etc 
+//=> and functions in vaccine like howToTake() and preservationDetails(), upon execution it should give us desired info like how many doses and what temperature to use
+//=> give me an example of callStack - and blow up the stack
+//=> how functions are special case in javascript, try giving an example by creating a class using functions - Parent should be Person with some details 
+//   and Inherited function should be Employee that will have EmpID, EmpName, Designaiton, methods like GetSalary, GetPromotionDetails, GetAddress, 
+//   address should be property of Person class
