a0b0c225403b0a599cbbcee63b1a30e80eefbfbc
Ashish <ash.ishu025g@gmail.com>
2021-08-30 22:00:41 +0530

    destruncting

--- es6/destructuring.js
+++ es6/destructuring.js
@@ -28,8 +28,8 @@ let myList = [1,2,3,4,5];
 
 let [firstValue, ...restValue] = myList;
 
-console.log(firstValue)
-console.log(restValue)
+// console.log(firstValue)
+// console.log(restValue)
 
 //d.  swapping of values
 
@@ -37,15 +37,49 @@ let a =1, b= 2;
 
 [a,b] = [b,a]
 
-console.log(a +" "+ b)
+// console.log(a +" "+ b)
 
 
-//2. Object Destructuring
+//2. Object Destructuring : copies the values to the left side object when keys are matched
 
-//a. single level 
+//a. single level and nested objects
 
 let user = {
     name : "Myat",
     age : 21,
-    address : "7th street new york lane"
+    address : "7th street new york lane",
+    sessions : {
+        session1 : "Javascript",
+        session2 : "ES6",
+        session3 : "AWS"
+    }
 }
+
+//let address = user.address;
+//let name = user.name;
+//let age = user.age;
+//let session2 = user.sessions.session2
+
+let {name, age, address = "Default Address", sessions:{session2, session4 = "Java 17"}} = user
+
+console.log(address)
+console.log(name)
+console.log(age)
+console.log(session2)
+console.log(session4)
+//console.log(sessions)
+
+
+//question :
+
+let Student = {
+    firstname : "Put your name",
+    address : " ??",
+    sessionTopics : {
+        covered1 : "CoreJS",
+        covered2 : "OOJS"
+    }
+}
+
+//print firstname and sessionTopics, 
+//along with that also create a lastname and covered3 as "ES6", without making any change in student
