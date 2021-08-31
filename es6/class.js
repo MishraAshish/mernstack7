7bd5be2ea58bc6fbbc76cf1e69e31aaff72e8d8b
Ashish <ash.ishu025g@gmail.com>
2021-08-31 21:10:59 +0530

    question

--- es6/class.js
+++ es6/class.js
@@ -16,8 +16,15 @@ class Vaccination
             date : this.date
         }
     }
+
+    //Sqaure = () => this.length*this.length; //creating an arrow function    
+    //Rectangle = () => this.length*this.width; //
 }
 
 let VccnObj = new Vaccination("Saranya", (new Date()).toDateString(), "7th Street New York City" )
 
 console.log(VccnObj.getSchedule())
+
+// create a class with name calculator, which accepts two params in cnstrctr - should have methods
+// add, subTract, Multiply and divide and some level of validation
+// create functions using arrow only
