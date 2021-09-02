b0e454e257fdc24763aa1aee2afdecd9f1afd21f
Ashish <ash.ishu025g@gmail.com>
2021-09-02 21:57:31 +0530

    es6assesment

---
+++ es6/es6assesment.js
@@ -0,0 +1,36 @@
+// 1. How to preserve the immutability on my heroes list? Solve below problems using the same
+// a. Get heroes who are not evils
+// b. Print Unique family names
+// c. Print Hero Names from given objects, and append sir in each of them before printing
+
+// const heroes = [
+//   { name: 'Wolverine',      family: 'Marvel',    isEvil: false },
+//   { name: 'Deadpool',       family: 'Marvel',    isEvil: false },
+//   { name: 'Magneto',        family: 'Marvel',    isEvil: true  },
+//   { name: 'Charles Xavier', family: 'Marvel',    isEvil: false },
+//   { name: 'Batman',         family: 'DC Comics', isEvil: false },
+//   { name: 'Harley Quinn',   family: 'DC Comics', isEvil: true  },
+//   { name: 'Legolas',        family: 'Tolkien',   isEvil: false },
+//   { name: 'Gandalf',        family: 'Tolkien',   isEvil: false },
+//   { name: 'Saruman',        family: 'Tolkien',   isEvil: true  }
+// ]
+
+//2. Give me an example of map and set collection each
+
+//3. Create a promise object that get resloved after two seconds and rejected after three. Also it returns 5 ES6 features on resolved
+
+//4. Using the promise object at #3 create an async and await feature
+
+//5. Use the spread and rest operator to create a function which can multiple numbers from 1...n (n is the number of choice)
+
+//6. Print the last name through destructuring and add a contact number:9119119110 as well
+// const person = {
+//     userDetails :{
+//         first: "FirstName",
+//         last: "LastName"
+//     }
+// }
+
+//7. Give me an example of const data manipulation
+
+//8. What is the difference between for-of and for-in show with examples
