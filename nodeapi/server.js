bb3afd09c6d6e98bb6577c34c308e68383df8ffe
Ashish <ash.ishu025g@gmail.com>
2021-09-08 21:28:58 +0530

    query string and api setup

--- nodeapi/server.js
+++ nodeapi/server.js
@@ -1,19 +1,33 @@
 console.log("First log in node api")
 
-const express = require('express');
+const express = require('express'); // imported express module or top level class of express
 const port = 9090;
-const app = express();
+const app = express(); //by invoking top level class we are initilizing the application
 
-app.get('/', function (req, res) {
-    res.send('Hello World')
+app.get('/', (req, res)=>{
+
+    res.json({
+        name : "Saranya",
+        place : "Somewhere in world"
+    })
+})
+
+app.get('/SaveUser', (req, res)=>{ //http://localhost:9090/SaveUser?name=testme&address=usa
+    let queryObject = req.query;
+    console.log("queryObject ", queryObject)
+    res.json(queryObject)
 })
 
 app.get('/Hi', function (req, res) {
     res.send('I am new to nodemon World')
 })
 
-console.log(`we are listening on port ${port}`)
+console.log(`we are listening on port ${port} with url http://localhost:9090`)
 
 app.listen(port)
 
 //ctrl+c to exit from loop
+
+//create a new setup of nodeapi with express, create one api savestudent that takes values in query string 
+//and saves it into a file and upon save success return the same data
+//name,session, age, interestarea
