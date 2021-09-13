8b72a3a58923cca0fce3d86dfcc8c0da0e65ba5e
Ashish <ash.ishu025g@gmail.com>
2021-09-13 23:24:46 +0530

    first react app

---
+++ reactapp/src/app/app.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default class AppComponent extends React.Component {
+
+
+    render(){
+        return(
+            <div>
+                <h1>This Component is loaded from react app Component</h1>
+
+                <button>I am button!</button>
+            </div>
+        )
+    }
+
+}
--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -1,3 +1,11 @@
 console.log("This is hello world from console!")
 
-alert("Hello world on alert!")
+import React from "react";
+import {render} from "react-dom";
+
+import AppComponent from "./app/app";
+
+render(
+    <AppComponent/>, //bootstrapping the application component to show on main page
+    document.getElementById("root") //finding the root element that acts as a container
+)
