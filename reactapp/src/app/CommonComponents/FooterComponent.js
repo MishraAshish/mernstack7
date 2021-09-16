0a30c2bc6b57569ae9d05d5954e8d210eabddddd
Ashish <ash.ishu025g@gmail.com>
2021-09-16 22:05:25 +0530

    questions

--- reactapp/src/app/CommonComponents/FooterComponent.js
+++ reactapp/src/app/CommonComponents/FooterComponent.js
@@ -2,6 +2,7 @@ import React, {Fragment} from "react";
 
 
 let Footer = (props)=>{
+    //props are readonly objects which child can use but not modify
     //props.children - array of elements passed from parent as child
     return(
         <Fragment>
@@ -14,3 +15,10 @@ let Footer = (props)=>{
 }
 
 export default Footer;
+
+//Create - a state in app or any parent class component (bestfriend)
+//Create child functional component with DummyName
+//To Dummy component pass the bestfriend name as props
+//Also try to change the name on click event (in app js) 
+
+//Also create functional components to receive props inline and as child
