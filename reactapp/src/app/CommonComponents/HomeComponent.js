bad08968513c11e2b3d3fa6751e28e9a8fe2dbe9
Ashish <ash.ishu025g@gmail.com>
2021-09-17 22:14:28 +0530

    set sate and event binding

--- reactapp/src/app/CommonComponents/HomeComponent.js
+++ reactapp/src/app/CommonComponents/HomeComponent.js
@@ -8,17 +8,37 @@ export default class Home extends Component {
     
     constructor(props){
         super(props);
+
+        this.state = {
+            searchParam : "I have not searched anything"
+        }
+    }
+
+    searchFunction = (evt) => {
+        let target = evt.target;
+        //console.log("target ", target);
+        //this.state.searchParam = target.value;
+
+        this.setState({ // a callback api provided by react to communicate to react rendering engine that it needs re-render
+            searchParam : target.value
+        }) //being a call back we don't see the changes immediatly
+
+        //console.log("searchParam ", this.state.searchParam);
     }
 
     render(){
         let footerMsg = "I am footer for this application";
 
+        console.log("Render Home")
+
         return(
             <Fragment>
-                <Header>
-
-                </Header>
+                <Header />
                 <h1>This is my Home Component for - {this.props.sessionName}</h1>
+
+                <input type="text" value={this.state.searchParam} placeholder="Please type something to search" 
+                    onChange={this.searchFunction}/>
+
                 <Footer msg={footerMsg} show={this.props.showChildren}>
                     <h4>Child Element 2</h4>
                     <h4>Child Element 3</h4>
