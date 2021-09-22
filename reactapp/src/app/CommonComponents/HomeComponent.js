fc94b610004c8eb64eb9e269c88164a0594302d6
Ashish <ash.ishu025g@gmail.com>
2021-09-22 20:44:14 +0530

    questions

--- reactapp/src/app/CommonComponents/HomeComponent.js
+++ reactapp/src/app/CommonComponents/HomeComponent.js
@@ -180,13 +180,3 @@ export default class Home extends PureComponent {
 
 
 
-//Create a component with your name (first)
-//Show all lifecycle hooks that have been discussed
-//Create another example with Pure component
-//Make that pure component child of first component
-//Pass a event/function as a prop to Child component and use it to send data back, and show update 
-//use ref keyword to access one element in first component
-
-//create navigations for your own name page
-// also move to that page on button click
-// make a route with path params in it
---
+++ reactapp/src/app/CommonComponents/ReactQuestions.js
@@ -0,0 +1,22 @@
+//22/09/2021
+
+// create a user class component with state 
+// create react ref elements likes - name, address, age and seesionName
+// use ref elements to create controlled component
+// upon form submit set the values from these components to the state
+// show the user typed values from state to multiple labels 
+// list down benefits of controlled over uncontrolled components
+
+//21/09/2021
+//create navigations for your own name page
+// also move to that page on button click
+// make a route with path params in it
+
+//20/09/2021
+//Create a component with your name (first)
+//Show all lifecycle hooks that have been discussed
+//Create another example with Pure component
+//Make that pure component child of first component
+//Pass a event/function as a prop to Child component and use it to send data back, and show update 
+//use ref keyword to access one element in first component
+
