57d56536c9e665718b4ea48b672c3b12cb17edd2
Ashish <ash.ishu025g@gmail.com>
2021-10-08 21:01:57 +0530

    project question

--- reactapp/src/app/CommonComponents/PracticeQuestion.js
+++ reactapp/src/app/CommonComponents/PracticeQuestion.js
@@ -1,4 +1,16 @@
 
+// Review Page - 10/08/2021
+// This should get its reviews from recent orders page
+// User should be allowed to give ratings and his comments to each products
+// Upon successful submission each product should have a link to show its review 
+// When user expands product detail we should also see the button to which will take us to its review
+// on recent order page we can show a popup to submit rating or a new page its up to you //can use -> react bootstrap
+// user should only be able to give rating once cancel button is gone
+
+// Reorder - 10/08/2021
+// User to reorder from recent orders or from cancelled orders
+// A Simple process just add the order to your cart and replace or merge whatever is present in cart
+
 // Upon Cancel Click - 07/10/2021
 // Save the order to CancelledOrders collection (should have userid, cancelled, dateTime)
 // Make API to Save and Fetch from CancelledOrders
