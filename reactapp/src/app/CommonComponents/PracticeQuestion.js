f7aed793ec0346650f217711870426f2cd15da38
Ashish <ash.ishu025g@gmail.com>
2021-10-11 21:05:11 +0530

    notifications

--- reactapp/src/app/CommonComponents/PracticeQuestion.js
+++ reactapp/src/app/CommonComponents/PracticeQuestion.js
@@ -1,4 +1,23 @@
 
+// Notifications : 11/10/2021
+
+// Show notification on top of header (Right Corner)
+// Design Should show a circular or any small icon, to show number of notifications
+// Upon click it should expand in rectangular box and show all the notification messages 
+// All notification messages should be clickable
+// Upon click of any of them it should reduce the count in notification icon
+// It should have two types of notifications - static and dynamic
+// Static Notifications To Assist User
+// 1. To Add Products from Product Screen
+// 2. To Add Items from Cart Page
+// 3. To review cart from Checkout Page
+// 4. To Make Payment from Payment Page
+// 5. To Assist Them for cancel/reorder
+// Dynamic Notifications To Assist User
+// 1. To Check the number of items added in the Cart
+// 2. If user cancels an order it should it should add one notification that an order has been cancelled and add it to notification row
+
+
 // Review Page - 08/10/2021
 // This should get its reviews from recent orders page
 // User should be allowed to give ratings and his comments to each products
